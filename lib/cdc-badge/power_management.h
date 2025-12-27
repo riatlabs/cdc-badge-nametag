b366e7b2ec8631de70f58151abda651bd8a9cd71
Pavel Polach <pavel.polach@tropicsquare.com>
2025-12-27 13:48:57 +0100

    feat: Add power down when FLASH button is pressed

--- lib/cdc-badge/power_management.h
+++ lib/cdc-badge/power_management.h
@@ -5,22 +5,12 @@
 // Returns true on success, false on failure
 bool power_management_init();
 
-// Disable charging on BQ25895
-// If disconnect_battery is true, also turns off BATFET (disconnects battery - will lose power!)
-// Returns true on success, false on failure
-bool power_management_disable(bool disconnect_battery = false);
-
 // Disconnect battery (system stays powered through USB only)
 // Returns true on success, false on failure
 bool power_management_disconnect_battery();
 
-// Reconnect battery (re-enable BATFET)
-// Returns true on success, false on failure
-bool power_management_reconnect_battery();
-
-// Software shutdown via QON emulation (BATFET_DLY)
-// This emulates pulling QON low - delays then disconnects battery
-// Returns true on success, false on failure
-bool power_management_shutdown();
+// Process power button interrupt (GPIO0/FLASH_BTN_PIN)
+// Returns true if button was pressed, false otherwise
+bool powerdown_process_irq();
 
 #endif
